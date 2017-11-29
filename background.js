981df415ff911e7529003db9f859865352822d2c
JoaoBGusmao <joao@natsce.com>
2017-11-29 20:33:44 -0200

    Initial commit

---
+++ background.js
@@ -0,0 +1,12 @@
+// Autor: João Gusmão - http://github.com/joaobgusmao
+// De quem foi a ideia e colocar aquele homem digitando? O que ele tá digitando, afinal?
+// Porra, isso me deixa aflito toda vez que entro na plataforma, que ela fica instável e eu tenho que ficar vendo esse
+// praga digitando alguma coisa. Não, alguém tinha que arrancar esse homeme dai.
+// A FoxBit acaba de ficar 100% melhor
+
+document.querySelector('.home-banner__table').style.position='relative'
+var shitVideo = document.querySelector('.home-media');
+shitVideo.innerHTML = '';
+shitVideo.style.background = 'url(https://foxbit.com.br/Content/images/bg.jpg)';
+shitVideo.style.backgroundPosition = 'center';
+shitVideo.style.backgroundSize = 'cover';
